3b81d471d0946148f7e55f41c6a04e9a0c24e50b
Mogesi <thedononzito@gmail.com>
2026-06-01 14:00:12 +0300

    A beginner banking application with optimal functionality

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,90 @@
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
+
 public class Main {
-    public static void  main(String[] arg){
-        Scanner scanner=new Scanner(System.in);
-       /* String name="Bro code";
-        int gta=6;
-        double pi=3.142617;
-        char gender='M';
-        boolean isAdmin=true;
+  static Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    // JAVA BANKING PROGRAM
+
+    double balance= 0;
+    int choice;
+    boolean isRunning = true;
+
+    while (isRunning) {
+
+      System.out.println("==============");
+      System.out.println("SELECT BANKING OPTION (1-4)");
+      System.out.println("1.BALANCE");
+      System.out.println("2.DEPOSIT");
+      System.out.println("3.WITHDRAW");
+      System.out.println("4.EXIT");
+      System.out.println("===============");
+      System.out.print("Enter your choice: ");
+      choice = scanner.nextInt();
+      System.out.println("=================");
 
 
-        System.out.println(name +" is playing gta "+gta);
-        Scanner scanner=new Scanner(System.in);
-
-        System.out.println("Enter your name: ");
-        String name=scanner.nextLine();
-
-        System.out.println("Enter you age: ");
-        int age=scanner.nextInt();
-
-       System.out.println("What is your gpa:");
-        double gpa=scanner.nextDouble();
-
-        System.out.println("Are you a student?(true/false): ");
-        boolean isStudent=scanner.nextBoolean();
-
-        System.out.println("Hello " + name);
-        System.out.println("You are "+ age +" years old");
-        System.out.println("Your gpa is: "+ gpa);
-
-        if(isStudent){
-            System.out.println("You are enrolled in classes!");
-
-        }else{
-            System.out.println("You are not enrolled in classes!");
-        }*/
-        String noun1;
-        String adjective1;
-        String verb1;
-        String noun2;
-        String adjective2;
-        String verb2;
-
-        System.out.print("Enter a noun:");
-        noun1=scanner.nextLine();
-        System.out.print("Enter a verb(ending with -ing):");
-        verb1=scanner.nextLine();
-        System.out.print("Enter an adjective(noun descriptors):");
-        adjective1=scanner.nextLine();
-        System.out.print("Enter an adjective(noun descriptors):");
-        adjective2=scanner.nextLine();
-        System.out.print("Enter a verb(ending with -ing):");
-        verb2=scanner.nextLine();
-        System.out.print("Enter a noun:");
-        noun2=scanner.nextLine();
+     switch (choice) {
+        case 1 ->balance(balance);
+        case 2 -> balance+=deposit();
+        case 3 -> balance-=withdraw(balance);
+        case 4 -> isRunning = false;
+        default -> System.out.println("Invalid input!!TRY AGAIN");
+      }
 
 
 
 
-        System.out.println("If your "+ noun1 + " can spend three hours"+ verb1 +" how everything falls apart,it can spend three hours imagining how everything comes together.");
-        System.out.println("But we're " + adjective1 +" to worst case scenarios because they feel more real,more "+ adjective2 +",when really they are just mental abuse.");
-        System.out.println("Your "+verb2 +"muscle is actually your manifestation muscle running in"+noun2+",same engineering,wrong direction.");
-        System.out.println("As a man thnketh in his heart, so is he... ");
+    }
+    System.out.println("Thank you for banking with us!!😁");
+    scanner.close();
 
+  }
+  static void balance(double balance){
+    System.out.println("==============");
+    System.out.printf("$%.2f\n",balance);
 
-        scanner.close();
+  }
+  static double deposit(){
+    double amount;
+    System.out.println("Enter amount you wish to deposit :" );
+    amount=scanner.nextDouble();
+
+    if(amount<0){
+      System.out.println("Amount cannot be less than zero");
+      return 0;
+    }
+    else{
+      return amount;
     }
 
+  }
+  static double withdraw(double balance){
+    double amount;
+    System.out.println("Enter amount you wish to withdraw");
+    amount=scanner.nextDouble();
+
+    if(amount>balance){
+      System.out.println("==============");
+      System.out.println("INSUFFICIENT FUNDS");
+      return 0;
+    }
+    else if(amount<0){
+      System.out.println("==============");
+      System.out.println("Amount cannot be negative.Please try a different amount");
+      return 0;
+
+    }
+    else {
+      System.out.println("==============");
+      System.out.println("Withdrawal successful");
+
+      return amount;
+    }
+  }
 
 
 }
+
+
