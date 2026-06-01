1b573a605e21a7efdd0c8d6f61c5b61db7030d99
Mogesi <thedononzito@gmail.com>
2026-06-01 09:46:48 +0300

    Simple weight conversion program in Java!

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,34 @@
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
 public class Main {
     public static void  main(String[] arg){
         Scanner scanner=new Scanner(System.in);
-       //COMPOUND INTEREST CALCULATOR
 
-        int years;
-        Double principal;
-        Double numOfTimesCompounded;
-        Double rate;
-        Double amount;
+        double weight;
+        double newWeight;
+        int choice;
 
-        System.out.print("Enter the principal amount :");
-        principal=scanner.nextDouble();
 
-        System.out.print("Enter the number of times compounded: ");
-        numOfTimesCompounded=scanner.nextDouble();
+        System.out.println("1.Convert lbs to kgs");
+        System.out.println("2.Convert lbs to kgs");
+        System.out.print(" SELECT AN OPTION! ");
+        choice=scanner.nextInt();
 
-        System.out.print("Enter the interest rate(in % form):  ");
-        rate=scanner.nextDouble()/100;
 
-        System.out.print("Enter the number of years :");
-        years=scanner.nextInt();
+        if(choice==1){
+            System.out.println("Convert lbs to kgs: ");
+            weight=scanner.nextDouble();
+            newWeight=weight*0.45;
+            System.out.printf("Your weight is %.2f kgs",newWeight);
+        }else if(choice==2){
+            System.out.print("Convert kgs to lbs : ");
+            weight=scanner.nextDouble();
+            newWeight=weight/0.45;
+            System.out.printf("Your weight is %.2f lbs",newWeight);
+        }
+        System.out.println("code completed successfully!!");
+        System.out.println("see you again");
 
-        amount =principal*Math.pow(1 + rate/numOfTimesCompounded,numOfTimesCompounded*years);
-        System.out.println("The total amount is : " +amount );
 
 
         scanner.close();
